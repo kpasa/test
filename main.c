c20f6e36626712cd9f9badf037ad15ed6ec884f2
Kenan Pasanbegovic <kenan.pasanbegovic@gmail.com>
2020-11-21 16:13:08 +0100

    init commit
    Signed-off-by: Kenan Pasanbegovic <kenan.pasanbegovic@gmail.com>

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "Rectangle.h""
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	Rectangle_t rect;
+	
+	rect.a = 3;
+	rect.b = 4;
+	
+	printf("Rectangle area: %ld\n",Rectangle_Area(&rect));
+	
+	
+	printf("Hallo world! \n");
+	return 0;
+}
